9dfd2b94e7f951326cd55ac1fd47fb3580d3633e
minhduy-spec <duy225340@student.nctu.edu.vn>
2025-05-30 17:13:32 +0700

    updatte file

---
+++ baitap5.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 1. Hello <tên người đăng nhập>
+echo "Hello $USER"
+echo "Thư mục hiện hành: $(pwd)"
+echo "Danh sách tất cả tập tin và thư mục (kể cả ẩn):"
+ls -la
+echo "Ngày và giờ hiện tại: $(date)"
+bashfdjvsajfd
+nsafdjhvasjudf
+asdjbfbsaf
+
